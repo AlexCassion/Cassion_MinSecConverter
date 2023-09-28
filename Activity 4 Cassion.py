528249e6198da7d16d041eb70de08abbd2d79801
AlexCassion <145540115+AlexCassion@users.noreply.github.com>
2023-09-28 23:51:25 +0800

    Add files via upload

---
+++ Activity 4 Cassion.py
@@ -0,0 +1,5 @@
+seconds = int(input("Enter the amount of time in seconds: "))
+minutes = seconds // 60
+seconds2 = seconds % 60
+print(f"{seconds} seconds is equal to {minutes} minutes and {seconds2} seconds.")
+
